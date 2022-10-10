bb0040e72647bc80d8729fdaa8fafaf9b17ae50f
belladu0201 <belladu0201@gmail.com>
2022-10-10 00:00:11 +0000

    add

--- bash.sh
+++ bash.sh
@@ -1,6 +1,19 @@
+#!/bin/sh
 #!/usr/bin/env bash
 # bash-shell-variables
 # chmod +x bash.sh
+# ./bash.sh 
+echo "What do you think about Kanye West's lryrics? Positive or Negative? "
+read ANS
+if [ "$ANS" = 'Positive' ]; then
+    echo "Probably Wrong Answer"
+elif [ "$ANS" = 'Negative' ]; then
+    echo "Should be Correct Answer"
+else
+    echo "${ANS} is a wrong input! Type Positive or Negative"
+fi
+
+echo "--------------------------------"
 export NAME="Start of IDS706 Project 2"
 echo $NAME
 echo "This is Beibei's" $NAME
