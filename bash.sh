f12dd97463fa5771e121dc7c5082f8b3af27a516
belladu0201 <belladu0201@gmail.com>
2022-10-10 02:32:04 +0000

    bash

--- bash.sh
+++ bash.sh
@@ -13,36 +13,14 @@ else
     echo "${ANS} is a wrong input! Type Positive or Negative"
 fi
 
-echo "--------------------------------"
+echo "---------START OF THE PROJECT-----------------------"
 # do echo l'done
 export NAME="Start of IDS706 Project 2"
 echo $NAME
 echo "This is Beibei's" $NAME
 
-echo "--------------------------------"
-# Read through each line of the text and I have commented all these out
-#!/bin/bash
-touch filtered_lyrics.txt
-filename='Kanye_West.txt'
-n=1
-# bad=0
-# good=0
-while read line; do
-    reading each line
-    #if [grep -c -e bitch -e nigga -e shit -e fuck $line]; then
-    if grep -q bitch "$line"; then
-        bad += $((bad+1))
-        $line >> filtered_lyrics.txt
-    # elif [grep -c -e bitch -e love -e time -e world -e $line];
-    elif grep -q love  "$line"; then
-        bad += $((bad+1))
-        $line >> filtered_lyrics.txt
-    fi
-    #echo "Line No. $n : $line" # I comment out the line to print each line of the lyrics
-    n=$((n+1))
-done < filtered_lyrics.txt
 #done < $filename
-
+echo "---------SOME COMMANDS-----------------------"
 ########## Radnomly shuffle 10 lines of the lyrics
 shuf -n 10 "$filename"
 grep -c bitch "$filename" 
@@ -58,3 +36,29 @@ grep -c -v -e love -e peace "$filename"
 ## The total number of lines in the Kanye_West.txt
 wc -l "$filename"
 
+echo "---------FILTER THE TEXT-----------------------"
+# Read through each line of the text and I have commented all these out
+#!/bin/bash
+touch filtered_lyrics.txt
+filename='Kanye_West.txt'
+n=1
+bad=0
+good=0
+while read line; do
+    reading each line
+    #if [grep -c -e bitch -e nigga -e shit -e fuck $line]; then
+    echo "Line No. $n : $line"
+    if grep -q bitch "$line"; then
+        bad += $((bad+1))
+        echo "Bad"
+        $line >> filtered_lyrics.txt
+    # elif [grep -c -e bitch -e love -e time -e world -e $line];
+    elif grep -q love  "$line"; then
+        bad += $((bad+1))
+        echo "Good"
+        $line >> filtered_lyrics.txt
+    fi
+    #echo "Line No. $n : $line" # I comment out the line to print each line of the lyrics
+    n=$((n+1))
+done < filtered_lyrics.txt
+echo "--------------------------------"
