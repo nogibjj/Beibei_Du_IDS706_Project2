e1d0414ba2d0e82195fd483f4c6571ac3dcf7dab
belladu0201 <belladu0201@gmail.com>
2022-10-10 03:14:50 +0000

    add

--- bash.sh
+++ bash.sh
@@ -22,6 +22,7 @@ echo "This is Beibei's" $NAME
 #done < $filename
 echo "---------SOME COMMANDS-----------------------"
 ########## Radnomly shuffle 10 lines of the lyrics
+filename='Kanye_West.txt'
 shuf -n 10 "$filename"
 grep -c bitch "$filename" 
 echo "There are 115 bitches in the Kanye_West.txt"
@@ -39,32 +40,46 @@ wc -l "$filename"
 echo "---------FILTER THE TEXT-----------------------"
 # Read through each line of the text and I have commented all these out
 #!/bin/bash
-touch filtered_lyrics.txt
-filename='Kanye_West.txt'
+touch filtered_lyrics_bad.txt
+touch filtered_lyrics_good.txt
 n=1
-bad=0
-good=0
-while read line
-do
-   echo $line
-   // or some_function "$line"
-done < testfile.txt
+# bad=0
+# good=0
+#!/bin/bash
+for f in 'Kanye_West.txt';
+    do 
+    grep -i "bitch" Kanye_West.txt >> filtered_lyrics_bad.txt;
+    grep -i "Love" Kanye_West.txt >> filtered_lyrics_good.txt;
+# while read line
+# do
+#    echo $line
+#    if grep -q "bitch" "$line";
+#     then
+#         echo "YES"
+#         #echo $line >> filtered_lyrics.txt
+#     elif grep -q "love" "$line";
+#     then
+#         echo "NO"
+#         #echo $line >> filtered_lyrics.txt
+#     fi
 
-while read line; do
-    reading each line
-    #if [grep -c -e bitch -e nigga -e shit -e fuck $line]; then
-        echo "Line No. $n : $line"
-    if grep -q bitch "$line"; then
-        bad += $((bad+1))
-        echo "Bad"
-        $line >> filtered_lyrics.txt
-    # elif [grep -c -e bitch -e love -e time -e world -e $line];
-    elif grep -q love  "$line"; then
-        bad += $((bad+1))
-        echo "Good"
-        $line >> filtered_lyrics.txt
-    fi
-    #echo "Line No. $n : $line" # I comment out the line to print each line of the lyrics
-    n=$((n+1))
-done < filtered_lyrics.txt
+done < Kanye_West.txt
+
+# while read line; do
+#     reading each line
+#     #if [grep -c -e bitch -e nigga -e shit -e fuck $line]; then
+#         echo "Line No. $n : $line"
+#     if grep -q bitch "$line"; then
+#         bad += $((bad+1))
+#         echo "Bad"
+#         $line >> filtered_lyrics.txt
+#     # elif [grep -c -e bitch -e love -e time -e world -e $line];
+#     elif grep -q love  "$line"; then
+#         bad += $((bad+1))
+#         echo "Good"
+#         $line >> filtered_lyrics.txt
+#     fi
+#     #echo "Line No. $n : $line" # I comment out the line to print each line of the lyrics
+#     n=$((n+1))
+# done < filtered_lyrics.txt
 echo "--------------------------------"
